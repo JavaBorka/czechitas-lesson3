73cfd9a913f6cb2a1593c7a286d8bd0c5c496156
JavaBorka <borka34@gmail.com>
2023-02-05 22:11:00 +0100

    Domácí úkol č.1

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 // tady je místo pro náš program
+
+let vstup = document.querySelector(".vstup");
+const jmeno = prompt("Zadej svoje jméno a přijímení");
+const rokNar = Number (prompt ("Zadej svůj rok narození"));
+let vek = 2023-rokNar;
+vstup.innerHTML = jmeno + " " + vek;
