28a603d8bb10a227b91ed47b4d49a3adef21a557
JavaBorka <borka34@gmail.com>
2023-02-05 22:40:59 +0100

    Domácí úkol č.2

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 // tady je místo pro náš program
 
+// DÚ1: 1. Vytvoř program, který se zeptá uživatele ("vstup"), na jeho Jméno, Příjmení a rok narození. Na výstupu vypiš jeho Jméno Příjmení a Věk. 
+
 let vstup = document.querySelector(".vstup");
 const jmeno = prompt("Zadej svoje jméno a přijímení");
 const rokNar = Number (prompt ("Zadej svůj rok narození"));
 let vek = 2023-rokNar;
 vstup.innerHTML = jmeno + " " + vek;
+
+// DÚ2: Přidej do programu "vstup" uživatele s jeho oblíbenou barvou ("anglicky") s tím, že celý výpis s úkolu č.1. obarvíš danou barvou, ze vstupu uživatele.
+
+const barva = prompt("Když máš rád modrou barvu, tak zadej blue");
+vstup.style.color = barva;
